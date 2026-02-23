a96b4c78ec948f7bccef4d8e19a5097b0376f7e8
mega-mind-sketch <megam8035@gmail.com>
2026-02-23 11:46:05 -0500

    Add first-agent code.

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [genai.types.Content(role="user", parts=[genai.types.Part(text=args.user_prompt)])]
+
+
+response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents=args.user_prompt
+    )
+
+prompt_token_count = response.usage_metadata.prompt_token_count if response.usage_metadata != None else 0
+response_token_count = response.usage_metadata.candidates_token_count if response.usage_metadata != None else 0
+
+
+
+def main():
+    # print("Hello from first-agent!")
+    # print()
+    
+    if args.verbose:
+
+        print("User prompt:", args.user_prompt)
+        print('Prompt tokens:', prompt_token_count)
+        print('Response tokens:', response_token_count)
+        print(response.text)
+    else:
+        print(response.text)
+        # print("Response: ")
+
+
+
+if __name__ == "__main__":
+    main()
